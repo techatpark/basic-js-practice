6c08d99733bc2a3cfeba1b1cd7d18b8d328ba6fd
dkarthicks27 <dkarthicks27@gmail.com>
2020-01-17 14:22:27 +0530

    if a given item is array

--- functional.js
+++ functional.js
@@ -1,6 +1,6 @@
 function mapForEach(arr, fn) {
   const newArray = [];
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     newArray.push(
       fn(arr[i])
     );
@@ -9,16 +9,18 @@ function mapForEach(arr, fn) {
 }
 
 function getSecondLargest(nums) {
-  nums.sort(function(a, b){return a-b});
+  nums.sort(function (a, b) { return a - b });
   let arr = nums.reverse();
   let index = arr[0];
-  for(let i=0; i<arr.length; i++){
-      if(arr[i]<index){
-          return arr[i];
-      }
-      else if(i===arr.length-1){
-        return arr[i];
-      }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < index) {
+      return arr[i];
+    }
+    else if (i === arr.length - 1) {
+      return arr[i];
+    }
   }
 }
-module.exports = {mapForEach,getSecondLargest};
+// function find if a given item is an array or not;
+const is_array = input => (toString.call(input) === "[object Array]") ? true : false;
+  module.exports = { mapForEach, getSecondLargest,is_array };
--- functional.test.js
+++ functional.test.js
@@ -10,3 +10,8 @@ test('find the second largest', () => {
 expect(x.getSecondLargest([6,6,6,6,6])).toBe(6);
 expect(x.getSecondLargest([10,6,3,10,10])).toBe(6);
 });
+
+test('find if a given item is an array', () => {
+expect(x.is_array('w3resource')).toBe(false);
+expect(x.is_array([1, 2, 4, 0])).toBe(true);
+});
