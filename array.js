bdd5a95f8495fb0716f678a633f56a6c2f8004c7
dkarthicks27 <dkarthicks27@gmail.com>
2020-01-19 01:36:25 +0530

    rotating an array

--- array.js
+++ array.js
@@ -1,18 +1,28 @@
 const greatest_sum = num => {
-    let sum= num[0]+num[1];
-    let position = [num[0],num[1] ];
-    for(let i=0; i<num.length; i++){
-        for(let j=i+1;j<num.length;j++){
-            if(num[i]+num[j]>sum){
-                sum =num[i]+num[j];
-                position[0]= num[i];
-                position [1]= num[j];           
+    let sum = num[0] + num[1];
+    let position = [num[0], num[1]];
+    for (let i = 0; i < num.length; i++) {
+        for (let j = i + 1; j < num.length; j++) {
+            if (num[i] + num[j] > sum) {
+                sum = num[i] + num[j];
+                position[0] = num[i];
+                position[1] = num[j];
             }
         }
     }
     return position;
 }
+//rotating an array
 
+const rotate = (arr, d) => {
+    let arr1 = [];
+    for (let i = 0; i < d; i++) {
+        arr1[i] = arr.shift();
+    }
+    for(let i=0;i<d;i++){
+        arr.push(arr1[i]);   
+    }
+    return arr;
+}
 
-
-module.exports = { greatest_sum};
+module.exports = { greatest_sum, rotate };
--- array.test.js
+++ array.test.js
@@ -4,3 +4,8 @@ test('find the greatest sum', () => {
 expect(x.greatest_sum([1,2,3,4])).toStrictEqual([3,4]);
 expect(x.greatest_sum([1,1,1,1,1])).toStrictEqual([1,1]);
 });
+
+test('rotate an array about whatever is said', () => {
+expect(x.rotate([1,2,3,4,5], 3)).toStrictEqual([4,5,1,2,3]);
+expect(x.rotate([1,1,1,1,1], 3)).toStrictEqual([1,1,1,1,1]);
+});
