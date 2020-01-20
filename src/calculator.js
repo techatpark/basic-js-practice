4f90502e656d08bacf65ee7e121beb70f65c3add
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-01-20 21:13:01 +0800

    prettier

--- src/calculator.js
+++ src/calculator.js
@@ -1,28 +1,26 @@
-const area = (a, b) => a * b;
+const area = (a, b) => a * b
 
-const perimeter = (a, b) => 2 * (a + b);
+const perimeter = (a, b) => 2 * (a + b)
 
 const factorial = a => {
-    return (a === 0 || a === 1) ? 1 : a * factorial(a - 1);
+    return a === 0 || a === 1 ? 1 : a * factorial(a - 1)
 }
 
 const multiply = (a, b) => {
-    return a * b;
+    return a * b
 }
 //function of fibannocci series before memoization
-const fib = n => n <= 1 ? n : fib(n - 1) + fib(n - 2);
+const fib = n => (n <= 1 ? n : fib(n - 1) + fib(n - 2))
 //function with memoization time
 const fib_ = n => {
-    let s = [];
+    let s = []
     if (n <= 1) {
-        return n;
+        return n
     }
     if (s[n] != undefined) {
-        return s[n];
+        return s[n]
+    } else {
+        return fib_(n - 1) + fib_(n - 2)
     }
-    else {
-        return fib_(n - 1) + fib_(n - 2);
-    }
-
 }
-module.exports = { area, perimeter, factorial, multiply, fib, fib_ };
+export default { area, perimeter, factorial, multiply, fib, fib_ }
