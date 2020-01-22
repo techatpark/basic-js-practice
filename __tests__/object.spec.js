69d59c4e1e7da534ddc1d137772b66040a00e669
dkarthicks27 <dkarthicks27@gmail.com>
2020-01-22 16:26:39 +0530

    objects properties sums

--- __tests__/object.spec.js
+++ __tests__/object.spec.js
@@ -1,27 +1,44 @@
 const x = require('../src/object');
 
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+let a = new Car('toyota', 12, 2009);
+let b = new Car('honda', 13, 1995);
+
 test('log the helloWorld program', () => {
     expect(x.helloWorld('Hello World')).toStrictEqual('Hello World');
 });
 
 test('Using Assign Function', () => {
-    function Car(make, model, year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
-    let a = new Car('toyota', 12, 2009);
-    let b = new Car('honda', 13, 1995);
     expect(x.Random(a, b)).toStrictEqual(b);
 });
 
 test('Using Objectdefineproperties', () => {
-    function Car(make, model, year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
-    let a = new Car('toyota', 12, 2009);
-    let b = new Car('honda', 13, 1995);
     expect(x.writable(a)).toBe("It is non writable property");
+});
+
+// displays all the enumerable properties
+test('checks for enumerable properties', () => {
+    expect(x.check_enumerable(b)).toStrictEqual(['make', 'model', 'year']);
+});
+
+//delete a particular property in an array
+test('delete the property in an object', () => {
+    expect(x.delet(a)).toStrictEqual(['make', 'model']);
+});
+// the following two test cases are for finding length of Object
+test('get the length of Object', () => {
+    expect(x.getlength(a)).toBe(3);
+});
+
+test('here we calculate the length of object', () => {
+    expect(x.getlength2(a)).toBe(3);
+});
+
+// so here i have to make an array of objects from the given objects
+test('we have to input objects and get array of objects', () => {
+    expect(x.arglength(a, b)).toBe(undefined);
 });
--- src/object.js
+++ src/object.js
@@ -1,12 +1,7 @@
 const helloWorld = x => {
     return x;
 }
-
-/*function Car (make, model, year){
-    this.make = make;
-    this.model = model;
-    this.year = year;
-}*/
+// Assign function named random for no reason
 const Random = (x, y) => {
     return Object.assign(x, y);
 }
@@ -26,10 +21,40 @@ const writable = a => {
             return "It is non writable property";
         }
         else {
-            return 2009 ;
+            return 2009;
         }
     }
 }
 
-//
-module.exports = { Random, writable };
+//deleting the year of Car in the object Car
+const delet = a => {
+    delete a.year;
+    return Object.keys(a);
+}
+
+const check_enumerable = a => {
+    return Object.keys(a);
+}
+// lets suppose we want to find the length of the object
+// it can be done in two ways
+// lets look at method 1
+
+const getlength = a => {
+    return Object.getOwnPropertyNames(a).length;
+}
+// yes  or you could also apply the method 2
+
+const getlength2 = a => {
+    return Object.keys(a).length;
+}
+
+// ok so lets see the use of arguments.length
+
+const arglength = (a, b) => {
+    const arr = [];
+    for (let i = 0; i < arguments.length; i++) {
+        arr.push(arguments[i]);
+    }
+    return arr;
+}
+module.exports = { arglength, Random, writable, check_enumerable, delet, helloWorld, getlength, getlength2 };
