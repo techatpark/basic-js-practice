f80612bfb89a82742ec1e8f5f9cef09b24218ba6
Sathish Kumar Thiyagarajan <sathishkumar.thiyagarajan@gmail.com>
2020-01-26 15:06:30 +0800

    literal added

--- __tests__/object-spec.js
+++ __tests__/object-spec.js
@@ -3,4 +3,7 @@ import { Person } from '../src/object';
 test('check Object creation', () => {
     const p = new Person('Sathish', 27, 'Male');
     expect(p.age).toBe(27);
+
+    expect(p.speak('tamil')).toBe('Sathish speaks tamil');
+    expect(p.friend('senthil')).toBe('Sathish has a friend named senthil');
 });
--- src/object.js
+++ src/object.js
@@ -2,6 +2,8 @@ function Person(name, age, gender) {
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.speak = language => `${this.name} speaks ${language}`;
+    this.friend = name => `${this.name} has a friend named ${name}`;
 }
 
 export { Person };
