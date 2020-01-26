25a48f86d2672bc58c0ef75d764ee3d7542acdd0
dkarthicks27 <dkarthicks27@gmail.com>
2020-01-26 12:48:31 +0530

    class added

--- src/class.js
+++ src/class.js
@@ -1,3 +1,14 @@
-class PersonClass {}
-
+class PersonClass {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    speak(language) {
+        return `${this.name} speaks ${language}`;
+    }
+    friend(name) {
+        return `${this.name} has a friend named ${name}`;
+    }
+}
 export { PersonClass };
